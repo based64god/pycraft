066cc4c78de3c7b1c3dc8734a8a4ccef412a45a6
Emmett Hitz <emmetthitz@gmail.com>
2018-04-05 20:39:18 -0400

    Rough implementation of base character classes

---
+++ src/classes/main.py
@@ -0,0 +1,6 @@
+class Base:
+    def __init__(self,abilities,stats,talents,buffs):
+        self.__abilities = dict(abilities)
+        self.__stats = dict(stats)
+        self.__talents = dict(talents)
+        self.__buffs = dict(buffs)
