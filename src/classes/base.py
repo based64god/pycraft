87f87a34546e7abf142f74c669635add8793dd41
Emmett Hitz <emmetthitz@gmail.com>
2018-04-05 21:04:29 -0400

    started on warrior

--- src/classes/base.py
+++ src/classes/base.py
@@ -1,4 +1,6 @@
-class Base:
+
+
+class BaseCharacter:
     def __init__(self,abilities,stats,talents,buffs):
         self.__abilities = dict(abilities)
         self.__stats = dict(stats)
---
+++ src/classes/warrior.py
@@ -0,0 +1,5 @@
+import base 
+
+class Warrior(base.BaseCharacter):
+    def __init__(self):
+        pass
